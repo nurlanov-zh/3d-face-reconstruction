36a5182f6326fc28985fe18bcf12a9bd0d4dd8cf
sirin <ge75piq@mytum.de>
2020-06-27 01:57:32 +0100

    removed scaling from procrustes algorithm

--- src/matching/include/sparse/procrustes.h
+++ src/matching/include/sparse/procrustes.h
@@ -20,83 +20,25 @@ class ProcrustesAligner
 		auto sourceMean = computeMean(sourcePoints);
 		auto targetMean = computeMean(targetPoints);
 
-		// Scaling
-		common::Matrix4f scaling =
-			computeScaling(sourcePoints, sourceMean, targetPoints, targetMean);
-		std::vector<common::Vector3f> scaledSourcePoints =
-			applyScaling(sourcePoints, scaling);
-
 		// Rotation
-		common::Matrix3f rotation = estimateRotation(
-			scaledSourcePoints, sourceMean, targetPoints, targetMean);
+		common::Matrix3f rotation = estimateRotation(sourcePoints, sourceMean,
+													 targetPoints, targetMean);
 
 		// Translation
 		common::Vector3f translation =
-			computeTranslation(sourceMean, targetMean, rotation, scaling);
+			computeTranslation(sourceMean, targetMean, rotation);
 
 		// Final transformation
 		common::Matrix4f estimatedPose;
 		estimatedPose.block(0, 0, 3, 3) = rotation;
 		estimatedPose.block(0, 3, 3, 1) = translation;
 		estimatedPose.row(3) << 0, 0, 0, 1;
-		estimatedPose = estimatedPose * scaling;
 
 		return estimatedPose;
 	}
 
    private:
-	std::vector<common::Vector3f> applyScaling(
-		const std::vector<common::Vector3f>& points,
-		const common::Matrix4f& scaling)
-	{
-		std::vector<common::Vector3f> scaledPoints = points;
-
-		// Convert Matrix4f to Matrix3f
-		common::Matrix3f scaling3f = scaling.block(0, 0, 3, 3);
-
-		// Transform points
-		for (int i = 0; i < points.size(); i++)
-		{
-			scaledPoints[i] = scaling3f * points[i];
-		}
-
-		return scaledPoints;
-	}
-
-	float computeMeanDistance(const std::vector<common::Vector3f>& points,
-							  const common::Vector3f& mean)
-	{
-		float avgdist = 0.0f;
-		float nPoints = points.size();
-
-		for (int i = 0; i < nPoints; i++)
-		{
-			avgdist += (mean - points[i]).norm();
-		}
-
-		avgdist = avgdist / nPoints;
-
-		return avgdist;
-	}
-
-	common::Matrix4f computeScaling(
-		const std::vector<common::Vector3f>& sourcePoints,
-		const common::Vector3f& sourceMean,
-		const std::vector<common::Vector3f>& targetPoints,
-		const common::Vector3f& targetMean)
-	{
-		common::Matrix4f scaling = common::Matrix4f::Identity();
-
-		float sourceAvgdist = computeMeanDistance(sourcePoints, sourceMean);
-		float targetAvgDist = computeMeanDistance(targetPoints, targetMean);
-
-		float scalingFactor = targetAvgDist / sourceAvgdist;
-		scaling = scalingFactor * scaling;
-		scaling(3, 3) = 1.0f;
-
-		return scaling;
-	}
-
+	
 	common::Vector3f computeMean(const std::vector<common::Vector3f>& points)
 	{
 		common::Vector3f mean = common::Vector3f::Zero();
@@ -143,14 +85,9 @@ class ProcrustesAligner
 
 	common::Vector3f computeTranslation(const common::Vector3f& sourceMean,
 										const common::Vector3f& targetMean,
-										const common::Matrix3f& rotation,
-										const common::Matrix4f scaling)
+										const common::Matrix3f& rotation)
 	{
-		// Convert Matrix4f to Matrix3f
-		common::Matrix3f scaling3f = scaling.block(0, 0, 3, 3);
-
-		common::Vector3f translation =
-			(-rotation * scaling3f * sourceMean) + targetMean;
+		common::Vector3f translation = (-rotation * sourceMean) + targetMean;
 
 		return translation;
 	}
